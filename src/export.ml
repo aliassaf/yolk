f92c454bb99b5e5d13a69806f16c60ff0d3d0189
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-07 17:30:51 +0100

    Locate a single library.

--- src/export.ml
+++ src/export.ml
@@ -1,6 +1,11 @@
 (** Main export functions **)
 
 let export_library ident =
+  let qualid = Libnames.qualid_of_ident ident in
+  let dirpath =
+    try Nametab.full_name_module qualid
+    with Not_found -> Error.unknown_module qualid
+  in
   ()
 
 let export_all () = ()
