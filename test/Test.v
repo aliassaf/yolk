71bcd86bab65fb018755668b330104fba4bdbd40
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-10 15:02:32 +0100

    Add various (counter)examples in the Test file to emphasize that only libraries can be exported.

--- test/Test.v
+++ test/Test.v
@@ -10,5 +10,21 @@ Cd "test".
 
 Yolk Library Peano.
 
+Yolk Library Coq.Init.Peano.
+
+Module M.
+
+End M.
+
+(* M is not a library. *)
+
+(*Yolk Library M.*)
+
+Module Peano1 := Peano.
+
+(* Peano1 is not a library. *)
+
+(*Yolk Library Peano1.*)
+
 Yolk All.
 
