884773b7aaf6958cb8b0ec6517c0ade58772a156
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-10 11:38:40 +0100

    Use require library when exporting a single library. This ensures that the reference points indeed to a library and that the library is loaded.

--- src/export.ml
+++ src/export.ml
@@ -2,29 +2,18 @@
 
 open Declarations
 
-let export_struct_expr_body formatter env struct_expr_body = ()
+let export_module_body formatter env module_body = ()
 
 (** Export the library referred to by [qualid].
     A libray is a module that corresponds to a file on disk. **)
 let export_qualified_library qualid =
-  (* Locate the module associated to the library. *)
-  let module_path =
-    try Nametab.locate_module qualid with
-    | Not_found -> Error.unknown_module qualid
-  in
-  (* Get the structure of the module. *)
+  let module_path = Nametab.locate_module qualid in
   let module_body = Global.lookup_module module_path in
-  let struct_expr_body =
-    match module_body.mod_expr with
-    | None -> failwith "empty library body"
-    | Some(struct_expr_body) -> struct_expr_body
-  in
-  (* Export the structure to a file of the same name.  *)
-  let env = Global.env () in
-  let out_channel = open_out (Names.string_of_mp module_path) in
+  let filename = Names.string_of_mp module_path in
+  let out_channel = open_out filename in
   let formatter = Format.formatter_of_out_channel out_channel in
   try
-    export_struct_expr_body formatter env struct_expr_body;
+    export_module_body formatter (Global.env ()) module_body;
     Format.pp_print_flush formatter ();
     close_out out_channel
   with e ->
@@ -32,8 +21,10 @@ let export_qualified_library qualid =
     close_out out_channel;
     raise e
 
+(** Export the library referred to by [reference]. *)
 let export_library reference =
-  let _, qualid = Libnames.qualid_of_reference reference in
+  let loc, qualid = Libnames.qualid_of_reference reference in
+  Library.require_library [loc, qualid] None;
   export_qualified_library qualid
 
 (** Export all loaded libraries. **)
