8eb2834a9aac12c696da36ff324aaf74bfea37d9
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-18 14:00:17 +0100

    Make not_supported use the common error function.

--- src/error.ml
+++ src/error.ml
@@ -2,12 +2,12 @@
 
 open Pp
 
-let not_supported feature =
-  failwith (Printf.sprintf "%s not supported" feature)
-
 let error message =
   Util.errorlabstrm "Yolk" message
 
 let unknown_module qualid =
   error (str "Module" ++ spc () ++ Libnames.pr_qualid qualid ++ spc () ++ str "not found.")
+   
+let not_supported feature =
+  error (str feature ++ str " not supported")
 
