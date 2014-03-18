2378f6d1d66545d242c05831fba4ffa4fa4a41c7
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-18 14:00:57 +0100

    Remove the unkown module error because it is not used anymore.

--- src/error.ml
+++ src/error.ml
@@ -5,9 +5,6 @@ open Pp
 let error message =
   Util.errorlabstrm "Yolk" message
 
-let unknown_module qualid =
-  error (str "Module" ++ spc () ++ Libnames.pr_qualid qualid ++ spc () ++ str "not found.")
-   
 let not_supported feature =
   error (str feature ++ str " not supported")
 
